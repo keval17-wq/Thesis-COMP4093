9fddeabaaf9c032520d0118aeb75be7e7f698bfd
Keval Gandhi <52366612+keval17-wq@users.noreply.github.com>
2025-04-15 06:12:41 +0000

    Add API key to configuration file

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
-API_KEY = ""
+API_KEY = "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"
 MODEL = "text-embedding-ada-002"
 DATA_FILE_PATH = "reviews_sample.txt"
 DATA_FILE_PATH2 = "reviews_sample490.txt"
